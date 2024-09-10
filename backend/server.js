e016b525f3ad911b55b661dd49275085cd830e97
AnnaKarolineNunes <annamiranda894@gmail.com>
2024-09-10 18:04:58 -0300

    Adicionado middleware CORS

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,12 @@ import express from 'express'  // Importa o framework Express para criar e geren
 import publicRoutes from './routes/public.js'  // Importa as rotas públicas (não exigem autenticação).
 import privateRoutes from './routes/private.js'  // Importa as rotas privadas (exigem autenticação).
 import auth from './middlewares/auth.js'  // Importa o middleware de autenticação para proteger as rotas privadas.
-import cors from 'cors'  // Importa o CORS para permitir que aplicações front-end acessem a API.
+import corsMiddleware from './middlewares/cors.js';  // Importa o CORS para permitir que aplicações front-end acessem a API.
 
 
 const app = express()  // Cria uma instância do Express para gerenciar o servidor e as rotas.
 app.use(express.json())  // Configura o servidor para interpretar requisições com corpo em formato JSON.
-app.use(cors())  // Habilita o CORS para permitir requisições de diferentes origens.
+app.use(corsMiddleware);
 
 app.use('/', publicRoutes)  // Define as rotas públicas, que não exigem autenticação.
 app.use('/', auth, privateRoutes)  // Define as rotas privadas, que exigem autenticação através do middleware 'auth'.
