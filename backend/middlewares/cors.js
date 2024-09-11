13f2377f0de30c2b80dd3b3f978b7d9c13dc67f6
AnnaKarolineNunes <annamiranda894@gmail.com>
2024-09-11 18:25:59 -0300

    modificação da url do forntend no cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -2,7 +2,7 @@ import cors from 'cors';
 
 // Configuração de CORS
 const corsOptions = {
-    origin: 'http://localhost:5173',  // ou a URL onde o frontend está hospedado
+    origin: 'https://taskly-gerenciador.netlify.app/',  // ou a URL onde o frontend está hospedado
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,  // Para permitir envio de cookies, se necessário
     optionsSuccessStatus: 204
